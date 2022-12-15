37331f5c1eb6799fd7166d00f4d701fa43dac372
Christopher <chris.brower.96@gmail.com>
2022-12-15 09:26:48 -0700

    commit tree 1ef01e21327cb7ce018c0d7f81e70d7fe2da15a8

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(x, y){
+    return x+y;
+}
+
+function subtract(x,y){
+    return x- y;
+}
+
+function multiply(x,y){
+    return x*y;
+}
+
+function divide(x,y){
+    return x/y;
+}
+
+function increment(x){
+    return ++x;
+}
+
+function decrement(x){
+    return --x;
+}
+
+function makeInt(x,y=10){
+    return parseInt(x,y);
+}
+
+function preserveDecimal(x){
+    return parseFloat(x);
+}
